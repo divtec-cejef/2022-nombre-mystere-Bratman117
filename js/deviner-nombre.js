45a357a267837bdbd5347d689afd3ca567a8f4bb
Brätman <93260228+Bratman117@users.noreply.github.com>
2022-08-30 15:53:29 +0200

    premier essai fini

--- js/deviner-nombre.js
+++ js/deviner-nombre.js
@@ -1,22 +1,37 @@
 /**
-* Jeu du nombre mystère
-* @author  Steve Fallet
-* @version 0.1
-* @since   2018-09-09 (date de création)
-*/
+ * Jeu du nombre mystère
+ * @author  Michaël Demars
+ * @version 0.1
+ * @since   2022-08-30 (date de création)
+ */
 
 // Main IIFE (Immediately-Invoked Function Expression, se prononce "iffy")
 (function main() {
-    "use strict";
+    'use strict';
+    const MIN = 1;
+    const MAX = 100;
 
     /**
      * Retourne un nombre entier aléatoire compris entre min et max
-     * @param min
-     * @param max
+     * @param min {number}
+     * @param max {number}
      * @returns {number}
      */
     function tireNombre(min, max) {
         return Math.floor(Math.random() * (max - min) + min);
     }
 
+    let nbrTire = tireNombre(MIN, MAX);
+    console.log(nbrTire);
+
+    let reponse = null;
+    let message = `Le nombre à deviner est compris entre ${MIN} et ${MAX}.`;
+
+    for (var nbrEssais = 0; nbrTire !== reponse; nbrEssais++) {
+        reponse = Number(prompt(message));
+        message = reponse > nbrTire ? 'C\'est moins' : 'C\'est plus';
+    }
+
+    alert(`Bravo, tu as gagné en ${nbrEssais} coups !`);
+    delete(nbrEssais);
 }()); // main IIFE
